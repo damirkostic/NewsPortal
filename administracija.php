be43ae3a29d5d761f97bc865701715b3bac52340
damirkostic <51851836+damirkostic@users.noreply.github.com>
2019-06-18 14:15:59 +0200

    Update administracija.php

--- administracija.php
+++ administracija.php
@@ -106,39 +106,39 @@
         while($row = mysqli_fetch_array($result)) {
             echo '<form action="" method="post" enctype="multipart/form-data">
             <div class="form-item">
-            <span id="porukaTitle" class="bojaPoruke"></span>
+            <span id="porukaTitle'.$row["id"].'" class="bojaPoruke"></span>
                 <label for="title" class="title">Naslov vijesti</label>
             <div class="form-field">
-                <input type="text" name="title" id="title" class="form-field-textual" value="'.$row['naslov'].'">
+                <input type="text" name="title" id="title'.$row["id"].'" class="form-field-textual" value="'.$row['naslov'].'">
             </div>
         </div>
         <div class="form-item">
             <label for="about">Kratki sadržaj vijesti (do 50 znakova)</label>
             <div class="form-field">
-            <span id="porukaAbout" class="bojaPoruke"></span>
-                <textarea name="about" id="about" cols="30" rows="10" class="form-field-textual">'.$row['sazetak'].'</textarea>
+            <span id="porukaAbout'.$row["id"].'" class="bojaPoruke"></span>
+                <textarea name="about" id="about'.$row["id"].'" cols="30" rows="10" class="form-field-textual">'.$row['sazetak'].'</textarea>
             </div>
         </div>
         <div class="form-item">
             <label for="content">Sadržaj vijesti</label>
-            <span id="porukaContent" class="bojaPoruke"></span>
+            <span id="porukaContent'.$row["id"].'" class="bojaPoruke"></span>
             <div class="form-field">
-                <textarea name="content" id="content" cols="30" rows="10" class="form-field-textual">'.$row['tekst'].'</textarea>
+                <textarea name="content" id="content'.$row["id"].'" cols="30" rows="10" class="form-field-textual">'.$row['tekst'].'</textarea>
             </div>
         </div>
         <div class="form-item">
-            <span id="porukaSlika" class="bojaPoruke"></span>
+            <span id="porukaSlika'.$row["id"].'" class="bojaPoruke"></span>
             <label for="pphoto" >Slika: </label>
             <div class="form-field">
-                <input type="file" name="pphoto" id="pphoto" class="input-text" accept="image/x-png,image/gif,image/jpeg" value="'.$row['slika'].'"> <br><img src="'.UPLPATH . $row['slika'].'">
+                <input type="file" name="pphoto" id="pphoto'.$row["id"].'" class="input-text" accept="image/x-png,image/gif,image/jpeg" value="'.$row['slika'].'"> <br><img src="'.UPLPATH . $row['slika'].'">
             </div>
         </div>
         <div class="form-item">
-            <span id="porukaKategorija" class="bojaPoruke"></span>
+            <span id="porukaKategorija'.$row["id"].'" class="bojaPoruke"></span>
             <label for="category">Kategorija vijesti</label>
             <div class="form-field">
-                <select name="category" id="category" class="form-field-textual" value="'.$row['kategorija'].'">
-                    <option value="0" disabled>Odaberite kategoriju</option>
+                <select name="category" id="category'.$row["id"].'" class="form-field-textual" value="'.$row['kategorija'].'">
+                    <option value="0" selected disabled>Odaberite kategoriju</option>
                     <option value="sport">Sport</option>
                     <option value="kultura">Kultura</option>
                 </select>
@@ -148,9 +148,9 @@
             <label>Spremiti u arhivu:
                 <div class="form-field">';
                     if($row['arhiva'] == 0) {
-                        echo '<input type="checkbox" name="archive" id="archive" /> Arhiviraj?';
+                        echo '<input type="checkbox" name="archive" id="archive'.$row["id"].'" /> Arhiviraj?';
                     } else {
-                        echo '<input type="checkbox" name="archive" id="archive" checked/> Arhiviraj?';
+                        echo '<input type="checkbox" name="archive" id="archive'.$row["id"].'" checked/> Arhiviraj?';
                     } 
                     echo '
                     </div>
@@ -160,67 +160,67 @@
                 <div class="form-item">
                     <input type="hidden" name="id" class="form-field-textual" value="'.$row['id'].'">
                     <button type="reset" value="Poništi">Poništi</button>
-                    <button type="submit" href="" name="update" id="slanje" value="Prihvati">Prihvati</button>
+                    <button type="submit" name="update"  id="slanje'.$row["id"].'" value="Prihvati">Prihvati</button>
                     <button type="submit" name="delete" value="Izbriši">Izbriši</button>
                 </div>
             </form>
             <script>
                 // Provjera forme prije slanja
-                document.getElementById("slanje").onclick = function(event) {
+                document.getElementById("slanje'.$row["id"].'").onclick = function(event) {
                 var slanjeForme = true;
                 // Naslov vjesti (5-30 znakova)
-                var poljeTitle = document.getElementById("title");
-                var title = document.getElementById("title").value;
+                var poljeTitle = document.getElementById("title'.$row["id"].'");
+                var title = document.getElementById("title'.$row["id"].'").value;
                 if (title.length < 5 || title.length > 30) {
                 slanjeForme = false;
                 poljeTitle.style.border="1px dashed red";
-                document.getElementById("porukaTitle").innerHTML="Naslov vjesti mora imati između 5 i 30 znakova!<br>";
+                document.getElementById("porukaTitle'.$row["id"].'").innerHTML="Naslov vjesti mora imati između 5 i 30 znakova!<br>";
                 } else {
                 poljeTitle.style.border="1px solid green";
-                document.getElementById("porukaTitle").innerHTML="";
+                document.getElementById("porukaTitle'.$row["id"].'").innerHTML="";
                 }
                 // Kratki sadržaj (10-100 znakova)
-                var poljeAbout = document.getElementById("about");
-                var about = document.getElementById("about").value;
+                var poljeAbout = document.getElementById("about'.$row["id"].'");
+                var about = document.getElementById("about'.$row["id"].'").value;
                 if (about.length < 10 || about.length > 100) {
                 slanjeForme = false;
                 poljeAbout.style.border="1px dashed red";
-                document.getElementById("porukaAbout").innerHTML="Kratki sadržaj mora imati između 10 i 100 znakova!<br>";
+                document.getElementById("porukaAbout'.$row["id"].'").innerHTML="Kratki sadržaj mora imati između 10 i 100 znakova!<br>";
                 } else {
                 poljeAbout.style.border="1px solid green";
-                document.getElementById("porukaAbout").innerHTML="";
+                document.getElementById("porukaAbout'.$row["id"].'").innerHTML="";
                 }
                 // Sadržaj mora biti unesen
-                var poljeContent = document.getElementById("content");
-                var content = document.getElementById("content").value;
+                var poljeContent = document.getElementById("content'.$row["id"].'");
+                var content = document.getElementById("content'.$row["id"].'").value;
                 if (content.length == 0) {
                 slanjeForme = false;
                 poljeContent.style.border="1px dashed red";
-                document.getElementById("porukaContent").innerHTML="Sadržaj mora biti unesen!<br>";
+                document.getElementById("porukaContent'.$row["id"].'").innerHTML="Sadržaj mora biti unesen!<br>";
                 } else {
                 poljeContent.style.border="1px solid green";
-                document.getElementById("porukaContent").innerHTML="";
+                document.getElementById("porukaContent'.$row["id"].'").innerHTML="";
                 }
                 // Slika mora biti unesena
-                var poljeSlika = document.getElementById("pphoto");
-                var pphoto = document.getElementById("pphoto").value;
+                var poljeSlika = document.getElementById("pphoto'.$row["id"].'");
+                var pphoto = document.getElementById("pphoto'.$row["id"].'").value;
                 if (pphoto.length == 0) {
                 slanjeForme = false;
                 poljeSlika.style.border="1px dashed red";
-                document.getElementById("porukaSlika").innerHTML="Slika mora biti unesena!<br>";
+                document.getElementById("porukaSlika'.$row["id"].'").innerHTML="Slika mora biti unesena!<br>";
                 } else {
                 poljeSlika.style.border="1px solid green";
-                document.getElementById("porukaSlika").innerHTML="";
+                document.getElementById("porukaSlika'.$row["id"].'").innerHTML="";
                 }
                 // Kategorija mora biti odabrana
-                var poljeCategory = document.getElementById("category");
-                if(document.getElementById("category").selectedIndex == 0) {
+                var poljeCategory = document.getElementById("category'.$row["id"].'");
+                if(document.getElementById("category'.$row["id"].'").selectedIndex == 0) {
                 slanjeForme = false;
                 poljeCategory.style.border="1px dashed red";
-                document.getElementById("porukaKategorija").innerHTML="Kategorija mora biti odabrana!<br>";
+                document.getElementById("porukaKategorija'.$row["id"].'").innerHTML="Kategorija mora biti odabrana!<br>";
                 } else {
                 poljeCategory.style.border="1px solid green";
-                document.getElementById("porukaKategorija").innerHTML="";
+                document.getElementById("porukaKategorija'.$row["id"].'").innerHTML="";
                 }
                 if (slanjeForme != true) {
                 event.preventDefault();
